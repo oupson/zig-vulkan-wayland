c9a9efb958b0cb8ee0364d252eb58a3069487515
oupson <oupson1er@gmail.com>
2025-02-07 13:41:16 +0100

    feat: use c allocator on release mode

--- src/main.zig
+++ src/main.zig
@@ -9,11 +9,37 @@ const Allocator = std.mem.Allocator;
 const App = @import("app.zig");
 const Renderer = @import("renderer.zig");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(gpa.deinit() == .ok);
+fn DebuggableAllocator() type {
+    if (builtin.mode == .Debug) {
+        return struct {
+            var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
-    const allocator = gpa.allocator();
+            fn allocator(_: *@This()) std.mem.Allocator {
+                return gpa.allocator();
+            }
+
+            fn deinit(_: *@This()) std.heap.Check {
+                return gpa.deinit();
+            }
+        };
+    } else {
+        return struct {
+            fn allocator(_: *@This()) std.mem.Allocator {
+                return std.heap.c_allocator;
+            }
+
+            fn deinit(_: *@This()) std.heap.Check {
+                return .ok;
+            }
+        };
+    }
+}
+
+pub fn main() !void {
+    var debugAllocator = DebuggableAllocator(){};
+    defer std.debug.assert(debugAllocator.deinit() == .ok);
+
+    const allocator = debugAllocator.allocator();
 
     var app = try App.init(allocator);
     defer app.deinit();
