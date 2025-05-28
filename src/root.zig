f987652d6294fedf3843405f00b09b61ce42458a
oupson <oupson1er@gmail.com>
2025-05-28 13:24:05 +0200

    chores: add brickmap to test

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const BrickMap = @import("brickmap.zig");
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test {
+    _ = BrickMap;
 }
